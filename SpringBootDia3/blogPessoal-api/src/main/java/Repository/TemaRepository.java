f51aa5aeacdfcccb1bde909c19d7e674426d56cf
unknown <giseledasilvac@gmail.com>
2020-11-06 21:17:12 -0300

    exercícios Spring dia 06/11

---
+++ SpringBootDia3/blogPessoal-api/src/main/java/Repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package Repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import generation.org.blogPessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String nome);
+	
+}
+
