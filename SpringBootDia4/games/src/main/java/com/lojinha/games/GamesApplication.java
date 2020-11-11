16cd1cb4b8cffc7bc1edc228e42d6398011e8210
unknown <giseledasilvac@gmail.com>
2020-11-10 22:44:00 -0300

    exercícios Spring dia 10/11

---
+++ SpringBootDia4/games/src/main/java/com/lojinha/games/GamesApplication.java
@@ -0,0 +1,13 @@
+package com.lojinha.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApplication.class, args);
+	}
+
+}
---
+++ SpringBootDia4/games/src/main/java/com/lojinha/games/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.lojinha.games.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.lojinha.games.model.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	
+}
